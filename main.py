87febab8462fcfee900729eddcbe34b93d2afa42
saramakosa <sara.makosa@student.h-farm.com>
2020-08-24 10:14:52 +0200

    Add argparse positional argument (days)

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from earthquakes_package import earthquakes
-import sys
+import argparse
 
-if len(sys.argv) > 1:
-    days = int(sys.argv[1])
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+            description="Get number of days from user",
+            epilog="Using FDSN	Web	Service	Specifications")
+    parser.add_argument("days", help='''The number of days in the past as a
+                        starting point for the research''')
+    parser.add_argument("--version", action="version", version="1.0")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    days = int(args.days) # string is not allowed
     mag, place = earthquakes.get_earthquake(days)
     print('The largest earthquake of last {} days had magnitude {} '
           'and was located at {}'.format(days, mag, place))
-
-else:
-    print("Please write number of days in the past to start searching for!")
-    exit()
